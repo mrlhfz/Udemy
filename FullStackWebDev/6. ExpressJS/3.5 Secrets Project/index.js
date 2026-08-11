6a639d12f92b3d2ed3aa397cc055276b4eff04cb
Amirul Hafiz <amirulhafizbc@gmail.com>
2026-08-11 21:11:52 +0800

    Learning 11/8/2026

--- FullStackWebDev/6. ExpressJS/3.5 Secrets Project/index.js
+++ FullStackWebDev/6. ExpressJS/3.5 Secrets Project/index.js
@@ -1,55 +1,30 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-
-//var
-var passwordTrue = false;
-
-//express + port
 import express from "express";
-//body parser 
-import bodyParser from "body-parser";
-//file path
-import {dirname} from "path";
-import {fileURLToPath} from "url";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 8008;
+const SECRET_PASSWORD = "mendeSia";
 
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.use(bodyParser.urlencoded({extended:true}));
-
-//password checker
-function passwordChecker(req, res, next) {
-    const password = req.body["password"];
-    if (password === "LOLOL") {
-        passwordTrue = true;
-    }
-    next();
-}
-app.use(passwordChecker);
-
-//morgan
-import morgan from "morgan";
-app.use(morgan("tiny"));
-
-//node
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/check", (req, res) => {
-    if(passwordTrue) {
-        res.sendFile(__dirname + "/public/secret.html");
-    } else {
-        res.redirect("/");
-    }
+  if (req.body.password === SECRET_PASSWORD) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.status(401).send("Incorrect password. <a href='/'>Try again</a>");
+  }
 });
-
-//listen
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
+  console.log(`Listening on port ${port}`);
 });
+
+

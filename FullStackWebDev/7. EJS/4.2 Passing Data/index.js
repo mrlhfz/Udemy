61e7fef72593b625fc381ef71b2376af37041c86
Amirul Hafiz <amirulhafizbc@gmail.com>
2026-08-14 23:01:06 +0800

    Learning 14/8/2026

--- FullStackWebDev/7. EJS/4.2 Passing Data/index.js
+++ FullStackWebDev/7. EJS/4.2 Passing Data/index.js
@@ -6,9 +6,21 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    length: null
+  });
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const fName = req.body.fName;
+  const lName = req.body.lName;
+  res.render("index.ejs", {
+    fName,
+    lName,
+    length: (fName + lName).length
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
